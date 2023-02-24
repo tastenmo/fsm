d1a166464629c7565b01c38f43276d1d93740ef3
heubuchm <martin.heubuch@escad.de>
2023-02-24 11:55:04 +0000

    hanging on TransitionTo things

--- include/new_fsm/state.h
+++ include/new_fsm/state.h
@@ -1,4 +1,11 @@
 /**
+ * @file state.h
+ * @author Martin Heubuch (martin.heubuch@escad.de)
+ * @brief
+ * @version 0.1
+ * @date 2023-02-23
+ *
+ * @copyright Copyright (c) 2023
  *
  */
 
@@ -7,8 +14,22 @@
 namespace escad {
 namespace new_fsm {
 
-template <typename Derived>
-struct state {
+/**
+ * @brief state base
+ *
+ * @tparam Derived
+ */
+
+template <typename Derived> struct state {
+
+  /**
+   * @brief Calls onEnter(const Event &event) ot Derived if it exists
+   *
+   * @tparam Target The Derived type
+   * @tparam Event The Event
+   * @param event
+   * @return decltype(std::declval<Target>().onEnter(event), void())
+   */
 
   template <typename Target = Derived, typename Event>
   auto enter(const Event &event)
@@ -16,13 +37,49 @@ struct state {
     static_cast<Target *>(this)->onEnter(event);
   }
 
+  /**
+   * @brief Calls onEnter() ot Derived if it exists
+   *
+   * @tparam Target The Derived type
+   * @return decltype(std::declval<Target>().onEnter(), void())
+   *
+   * @see https://arne-mertz.de/2017/01/decltype-declval/
+   */
   template <typename Target = Derived>
   auto enter() -> decltype(std::declval<Target>().onEnter(), void()) {
     static_cast<Target *>(this)->onEnter();
   }
 
+  /**
+   * @brief
+   *
+   * @param ...
+   */
   void enter(...) const noexcept {}
+
+  /**
+   * @brief Calls the transitionTo(const Event &event) of Derived if it exists
+   * 
+   * @tparam Target 
+   * @tparam Event 
+   * @tparam NewState 
+   * @param event 
+   * @return decltype(std::declval<Target>().template transitionTo<NewState>(event),
+   * std::optional<NewState>()) 
+   * 
+   * @remarks Does not work yet. The NewState return type should be replaced with std::variant<states ...> from the fsm ????
+   */
+
+  template <typename Target = Derived, typename Event, typename NewState>
+  auto transition(const Event &event)
+      -> decltype(std::declval<Target>().template transitionTo<NewState>(event),
+                  std::optional<NewState>()) {
+    return static_cast<Target *>(this)->template transitionTo<NewState>(event);
+  }
+
+  auto transition(...) const noexcept { return std::nullopt; }
+
 };
 
-}  // namespace new_fsm
-}  // namespace escad
+} // namespace new_fsm
+} // namespace escad
--- test/testNewFSM.cpp
+++ test/testNewFSM.cpp
@@ -1,87 +1,143 @@
-#include <functional>
-#include <utility>
-#include <iostream>
 #include <chrono>
+#include <functional>
+#include <iostream>
+#include <utility>
 
 #include <catch2/catch_test_macros.hpp>
 
 #include <new_fsm/state.h>
 
-
 using namespace escad::new_fsm;
-
 
 struct event1 {};
 struct event2 {
 
-    event2(int val): value_(val) {}
+  event2(int val) : value_(val) {}
 
-    int value_;
+  int value_;
 };
 struct event3 {};
 
-struct StateFirst : state<StateFirst>{
+using StateVariant = fsm::StateVariant;
 
-    StateFirst() : count1(0), value2(0) {}
+struct StateSecond;
+struct StateThird;
 
-    void onEnter();
 
-    void onEnter(const event1&){
+struct StateFirst : state<StateFirst> {
 
-        count1++;
+  StateFirst() : count1(0), value2(0) {}
 
-    } 
+  void onEnter(const event1 &) { count1++; }
 
-    void onEnter(const event2& ev){
+  void onEnter(const event2 &ev) { value2 += ev.value_; }
 
-        value2 += ev.value_;
 
-    } 
+  // Think about this, should not be here
+  //template <typename Event, typename NewState>
+  //std::optional<NewState> transitionTo(const Event &) {
 
-    int count1;
-    int value2;
+  //  return std::nullopt;
+  //}
 
+  
+  std::optional<StateVariant> StateFirst::transitionTo(const event1 &){
+
+    return StateSecond{};
+  }
+
+
+
+
+  //template<>
+  //auto transitionTo<StateSecond>(const event1 &);
+
+  int count1;
+  int value2;
 };
 
-struct StateSecond : state<StateSecond>{
+struct StateSecond : state<StateSecond> {
 
-    StateSecond() : count1(0) {}
+  StateSecond() : count1(0) {}
 
-    void onEnter(){
+  void onEnter() { count1++; }
 
-        count1++;
-
-    } 
-
-    int count1;
-
+  int count1;
 };
 
+struct StateThird : state<StateThird> {
 
+  StateThird() : count1(0) {}
 
+  int count1;
+};
 
-TEST_CASE("state_onEnter", "[new_fsm]"){
+TEST_CASE("state_onEnter", "[new_fsm]") {
 
+  StateFirst first;
+  StateSecond second;
+  StateThird third;
+
+  REQUIRE(first.count1 == 0);
+  REQUIRE(first.value2 == 0);
+  REQUIRE(second.count1 == 0);
+  REQUIRE(third.count1 == 0);
+
+  first.enter();
+  second.enter();
+  third.enter();
+
+  REQUIRE(first.count1 == 0);
+  REQUIRE(first.value2 == 0);
+  REQUIRE(second.count1 == 1);
+  REQUIRE(third.count1 == 0);
+
+  first.enter(event3{});
+  second.enter(event3{});
+  third.enter(event3{});
+
+  REQUIRE(first.count1 == 0);
+  REQUIRE(first.value2 == 0);
+  REQUIRE(second.count1 == 1);
+  REQUIRE(third.count1 == 0);
+
+  first.enter(event1{});
+  second.enter(event1{});
+  third.enter(event1{});
+
+  REQUIRE(first.count1 == 1);
+  REQUIRE(first.value2 == 0);
+  REQUIRE(second.count1 == 1);
+  REQUIRE(third.count1 == 0);
+
+  first.enter(event2{42});
+  second.enter(event2{42});
+  third.enter(event2{42});
+
+  REQUIRE(first.count1 == 1);
+  REQUIRE(first.value2 == 42);
+  REQUIRE(second.count1 == 1);
+  REQUIRE(third.count1 == 0);
+}
+
+TEST_CASE("state_transitionTo", "[new_fsm]"){
 
     StateFirst first;
 
-    REQUIRE(first.count1 == 0);
-    REQUIRE(first.value2 == 0);
 
-    first.enter(event3{});
+    auto second = first.transitionTo(event1{});
 
-    REQUIRE(first.count1 == 0);
-    REQUIRE(first.value2 == 0);
 
-    first.enter(event1{});
+    
+    
+    
+    
+    std::optional<StateSecond> second = first.transition(event1{});
 
-    REQUIRE(first.count1 == 1);
-    REQUIRE(first.value2 == 0);
-
-    first.enter(event2{42});
+    REQUIRE(second);
 
     REQUIRE(first.count1 == 1);
-    REQUIRE(first.value2 == 42);
+
 
 
 
