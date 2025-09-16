bd140645a536762436abc2a87d95d9af1bf8fd87
Martin Heubuch <martin.heubuch@spie-escad.de>
2025-09-16 21:25:20 +0000

    Refactor simple_fsm.cpp: streamline state definitions and improve transition handling

--- example/simple_fsm.cpp
+++ example/simple_fsm.cpp
@@ -3,19 +3,19 @@
 #include <iostream>
 #include <utility>
 
-#include <fsm/version.h>
 #include <fsm/state.h>
 #include <fsm/state_machine.h>
+#include <fsm/version.h>
 
-using namespace escad::new_fsm;
+using namespace spie::fsm;
 
 struct NoContext {};
 
 struct start {};
 struct pausing {
-  pausing(int val) : value_(val) {}
+   pausing(int val) : value_(val) {}
 
-  int value_;
+   int value_;
 };
 struct stop {};
 
@@ -26,113 +26,113 @@ struct Stopped;
 
 using States = states<Initial, Running, Paused, Stopped>;
 
-using Machine = StateMachine<States, NoContext &>;
+using Machine = StateMachine<States, NoContext>;
 
-struct Initial : state<Initial, Machine, NoContext> {
+struct Initial : state<Initial, Machine> {
 
-  using state<Initial, Machine, NoContext>::state;
+   using state<Initial, Machine>::state;
 
-  void onEnter() { std::cout << "Initial::onEnter()" << std::endl; }
+   void onEnter() { std::cout << "Initial::onEnter()" << std::endl; }
 
-  /**
-   * @brief transition to Running
-   *
-   * @return auto
-   */
-  auto transitionTo(const start &) { return sibling<Running>(); }
+   /**
+    * @brief transition to Running
+    *
+    * @return auto
+    */
+   auto transitionTo(const start &) { return transition<Running>(); }
 };
 
-struct Running : state<Running, Machine, NoContext> {
+struct Running : state<Running, Machine> {
 
-  using state<Running, Machine, NoContext>::state;
+   using state<Running, Machine>::state;
 
-  /**
-   * @brief onEnter
-   *
-   * @param start
-   */
-  void onEnter(const start &) {
-    std::cout << "Running::onEnter(cost start &)" << std::endl;
-  }
+   /**
+    * @brief onEnter
+    *
+    * @param start
+    */
+   void onEnter(const start &) {
+      std::cout << "Running::onEnter(cost start &)" << std::endl;
+   }
 
-  /**
-   * @brief transition to Paused
-   *
-   * @return auto
-   */
-  auto transitionTo(const pausing &) const { return sibling<Paused>(); }
+   /**
+    * @brief transition to Paused
+    *
+    * @return auto
+    */
+   auto transitionTo(const pausing &) const { return transition<Paused>(); }
 
-  /**
-   * @brief transition to Stopped
-   *
-   * @return auto
-   */
-  auto transitionTo(const stop &) const { return sibling<Stopped>(); }
+   /**
+    * @brief transition to Stopped
+    *
+    * @return auto
+    */
+   auto transitionTo(const stop &) const { return transition<Stopped>(); }
 };
 
-struct Paused : state<Paused, Machine, NoContext> {
+struct Paused : state<Paused, Machine> {
 
-  using state<Paused, Machine, NoContext>::state;
+   using state<Paused, Machine>::state;
 
-  /**
-   * @brief onEnter
-   *
-   * @param start
-   */
-  void onEnter(const pausing &) {
-    std::cout << "Paused::onEnter(cost pause &)" << std::endl;
-  }
+   /**
+    * @brief onEnter
+    *
+    * @param start
+    */
+   void onEnter(const pausing &) {
+      std::cout << "Paused::onEnter(cost pause &)" << std::endl;
+   }
 
-  /**
-   * @brief transition to Paused
-   *
-   * @return auto
-   */
-  auto transitionTo(const start &) const { return sibling<Running>(); }
+   /**
+    * @brief transition to Paused
+    *
+    * @return auto
+    */
+   auto transitionTo(const start &) const { return transition<Running>(); }
 
-  /**
-   * @brief transition to Stopped
-   *
-   * @return auto
-   */
-  auto transitionTo(const stop &) const { return sibling<Stopped>(); }
+   /**
+    * @brief transition to Stopped
+    *
+    * @return auto
+    */
+   auto transitionTo(const stop &) const { return transition<Stopped>(); }
 };
 
-struct Stopped : state<Stopped, Machine, NoContext> {
+struct Stopped : state<Stopped, Machine> {
 
-  using state<Stopped, Machine, NoContext>::state;
+   using state<Stopped, Machine>::state;
 
-  /**
-   * @brief onEnter
-   *
-   * @param start
-   */
-  void onEnter() { std::cout << "Stopped::onEnter()" << std::endl; }
+   /**
+    * @brief onEnter
+    *
+    * @param start
+    */
+   void onEnter() { std::cout << "Stopped::onEnter()" << std::endl; }
 };
-
-
 
 int main() {
 
-  std::cout << "A simple fsm..." << std::endl;
+   std::cout << "A simple fsm..." << std::endl;
 
-  std::cout << "Build with version: " << FSM_VERSION << std::endl;
+   std::cout << "Build with version: " << FSM_VERSION << std::endl;
 
-  NoContext ctx;
+   NoContext ctx;
 
-  //StateMachine sm(mpl::type_identity<States>{}, ctx); // create a state machine
-  auto sm = Machine(mpl::type_identity<States>{}, ctx); // create a state machine with reference context
-  sm.emplace<Initial>();
+   // StateMachine sm(mpl::type_identity<States>{}, ctx); // create a state
+   // machine
+   auto sm = Machine(mpl::type_identity<States>{},
+                     ctx); // create a state machine with reference context
+   sm.emplace<Initial>();
 
-  sm.dispatch(start{});
+   sm.dispatch(start{});
 
-  sm.dispatch(pausing{1});
+   sm.dispatch(pausing{1});
 
-  sm.dispatch(start{});
+   sm.dispatch(start{});
 
-  sm.dispatch(stop{});
+   sm.dispatch(stop{});
 
-  // Color entries: RED = -10 BLUE = 0 GREEN = 10
+   // Color entries: RED = -10 BLUE = 0 GREEN = 10
 
-  return 0;
+   return 0;
 }
