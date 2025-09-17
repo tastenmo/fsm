86194d079d2f7d6f37b8ea9ad71d5e684ce02d78
Martin Heubuch <martin.heubuch@spie-escad.de>
2025-09-17 14:32:53 +0000

    Add JSON test data and implement value handling in tests

---
+++ libs/json/test/value.cpp
@@ -0,0 +1,55 @@
+#include <chrono>
+#include <cstdint>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "core/utils.h"
+
+#include <json/value.h>
+
+using namespace spie::json;
+
+using namespace std::literals;
+
+auto myJsonPrinter = spie::overloaded{
+    [](number::JsonNumber &) { std::cout << "Number: " << std::endl; },
+    [](bool &value) { std::cout << "Bool: " << value << std::endl; },
+    [](std::string &value) { std::cout << "String: " << value << std::endl; },
+    [](jsonObject &) { std::cout << "Object: " << std::endl; },
+    [](jsonArray &) { std::cout << "Array: " << std::endl; },
+    [](auto) { std::cout << "unknown" << std::endl; }
+
+};
+
+TEST_CASE("Value - test_data", "[json]") {
+
+   std::ifstream ifs;
+
+   ifs.open("/workspaces/fsm/libs/json/test/data/1.json", std::ios::in);
+
+   REQUIRE(ifs);
+
+   std::string str(std::istreambuf_iterator<char>{ifs}, {});
+
+   view v(str);
+
+   value::Context ctx(v);
+
+   auto fsm = value::Machine(mpl::type_identity<value::States>{}, ctx);
+
+   fsm.emplace<value::Initial>();
+
+   REQUIRE(fsm.is_in<value::Finished>());
+
+   auto theObject = ctx.getValue();
+
+   REQUIRE(std::holds_alternative<jsonObject>(theObject));
+
+   std::visit(myJsonPrinter, theObject);
+}
